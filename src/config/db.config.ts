2784fd988f12cc12b56c9e7403448af596ad64c4
mediocre9 <mirzafahadzia9@gmail.com>
2025-12-14 16:04:49 +0500

    refactor: rename file `db.ts` to `db.config.ts`

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -3,12 +3,18 @@ import { isDevEnvironment } from "@utils/utils";
 import { Sequelize } from "sequelize";
 
 export const db = new Sequelize({
-  port: process.env.DB_PORT,
-  database: process.env.DB,
-  host: process.env.DB_HOST,
+  port: 3306,
+  database: "voyager_ota_dev",
+  host: "localhost",
   dialect: "mariadb",
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
+  username: "root",
+  password: "123",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
   logging: (msg) => Logger.debug(msg),
 });
 
