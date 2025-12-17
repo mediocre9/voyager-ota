1faddfb27df504fda34a8f40146503890ac97756
mediocre9 <mirzafahadzia9@gmail.com>
2025-12-18 01:55:49 +0500

    refactor: remove deprecated  module

--- src/services/cache.service.ts
+++ src/services/cache.service.ts
@@ -1,62 +1,6 @@
-/**
- *
- * ! Reconsider the cache strategy algorithms again and find the optimal suitable solution...
- */
 import { redis } from "@config/redis.connection.config";
 import { injectable } from "tsyringe";
 
-export enum SearchRotationMode {
-  REAR,
-  FORWARD,
-}
-
-/**
- * @deprecated This cache module has been deprecated!
- */
-@injectable()
-export class CacheManager {
-  /**
-   *
-   * @param key record's PublicId should be used
-   * @param value Any stringified object
-   */
-  public async prependToList(key: string, value: string): Promise<void> {
-    await redis.lpush(key, value);
-  }
-
-  public async readList(
-    key: string,
-    startOffset: number = 0,
-    endOffset: number = 5
-  ): Promise<string[]> {
-    return await redis.lrange(key, startOffset, endOffset);
-  }
-
-  public async isEmpty(key: string): Promise<boolean> {
-    return (await redis.llen(key)) === 0;
-  }
-
-  public async getListCount(key: string): Promise<number> {
-    return await redis.llen(key);
-  }
-
-  public async find(key: string): Promise<string[]> {
-    return await redis.lrange(key, 0, -1);
-  }
-
-  public async removeFromList(
-    key: string,
-    value: string,
-    mode: SearchRotationMode = SearchRotationMode.FORWARD
-  ): Promise<number> {
-    return await redis.lrem(key, mode, value);
-  }
-}
-
-/**
- *
- * !Reconsider the approach in detail.....
- */
 @injectable()
 export class CacheService {
   public async add(id: string, data: string): Promise<void> {
