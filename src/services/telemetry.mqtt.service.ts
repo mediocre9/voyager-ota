3d2f435a174587f635f8420953d5bcf10d9d96da
Mediocre <76650975+mediocre9@users.noreply.github.com>
2026-01-25 19:19:00 +0500

    Revert "refactor: use environment variables"
    This reverts commit e0730d0531a91a5d73c4459939e4f384f9b6cba9.

--- src/services/telemetry.mqtt.service.ts
+++ src/services/telemetry.mqtt.service.ts
@@ -2,7 +2,6 @@ import { Logger } from "@utils/logger";
 import mqtt from "mqtt";
 import { Server } from "socket.io";
 import { log } from "console";
-import { EnvConfig } from "@config/config";
 
 const io = new Server({
   transports: ["websocket"],
@@ -16,12 +15,12 @@ io.on("connection", (_) => {
   Logger.info("webscoket server up!");
 });
 
-const client = mqtt.connect(EnvConfig.MQTT_ADDRESS, {
+const client = mqtt.connect("mqtts://z8112f26.ala.asia-southeast1.emqxsl.com:8883", {
+  port: 8883,
   clean: true,
-  port: EnvConfig.MQTT_PORT,
-  clientId: EnvConfig.MQTT_CLIENT_ID,
-  username: EnvConfig.MQTT_USERNAME,
-  password: EnvConfig.MQTT_PASSWORD,
+  clientId: "voyager",
+  username: "voyager_telemetry_sub_service",
+  password: "123",
   connectTimeout: 4000,
   reconnectPeriod: 1000,
 });
