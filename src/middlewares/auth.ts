eedfb9670c15e7be12b1c66ae6cd7705d05b26f4
Mediocre <76650975+mediocre9@users.noreply.github.com>
2026-01-25 19:19:00 +0500

    Revert "feat: add `Auth` middleware"
    This reverts commit 319aa6793446ab448ca7cd36a9d3a7bbb908e849.

--- src/middlewares/auth.ts
+++
@@ -1,8 +0,0 @@
-import { NextFunction, Request, Response } from "express";
-
-export function isAuthenticated(request: Request, response: Response, next: NextFunction) {
-  if (request.isUnauthenticated()) {
-    return response.redirect("/");
-  }
-  next();
-}
